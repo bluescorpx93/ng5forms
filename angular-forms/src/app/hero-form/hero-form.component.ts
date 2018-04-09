176668442aae5852f3092321d2b6a815d5d0b386
Toufiq <burningraven06@gmail.com>
2018-04-09 23:37:43 +0600

    include Form classes for validation

---
+++ angular-forms/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
+@Component({
+	selector: 'app-hero-form',
+	templateUrl: './hero-form.component.html',
+	styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+	constructor() { }
+
+	powers = ['Really Smart', 'Super Flexible',
+	'Super Hot', 'Weather Changer'];
+	myModel = new Hero(18, 'Dr IQ', this.powers[0], 'Chucky Chuck');
+
+	submitted = false;
+
+	onSubmit(){
+		this.submitted = true;
+	}
+
+	ngOnInit() {
+	}
+
+	get diagnostic(){ return JSON.stringify(this.myModel)}
+
+}
